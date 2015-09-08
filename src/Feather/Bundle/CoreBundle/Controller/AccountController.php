494050f0ff1a9e823c1765cbc591a6d4db8f865c
Jonathan Gleyze <jonathan.gleyze.pro@gmail.com>
2015-09-08 14:56:40 -0400

    chore(Account): add feature for changing the password

--- src/Feather/Bundle/CoreBundle/Controller/AccountController.php
+++ src/Feather/Bundle/CoreBundle/Controller/AccountController.php
@@ -1,4 +1,25 @@
 <?php
+/*
+ * This file is part of Feather application.
+ *
+ * (c) William Rudent <william.rudent@gmail.com>
+ *
+ * Copyright © 2015 William Rudent <william.rudent@gmail.com>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the “Software”), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * The Software is provided “as is”, without warranty of any kind, express or implied, including but not
+ * limited to the warranties of merchantability, fitness for a particular purpose and noninfringement.
+ * In no event shall the authors or copyright holders be liable for any claim, damages or other liability,
+ * whether in an action of contract, tort or otherwise, arising from, out of or in connection with the
+ * software or the use or other dealings in the Software. »
+ */
 
 namespace Feather\Bundle\CoreBundle\Controller;
 
@@ -6,9 +27,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\Form\FormView;
 
-/* debug*/
-use Symfony\Component\HttpFoundation\Response;
+use FOS\UserBundle\Model\UserManagerInterface;
+
 /**
  * @Route("/account")
  * @Security("has_role('ROLE_USER')")
@@ -21,14 +43,24 @@ class AccountController extends Controller
      * @Route("/setting", name="setting")
      * @Template()
      *
-     * Load setting index
+     * Load setting index end create form
      *
-     * @author William Rudent <william.rudent@gmail.com>
+     * @author Jonathan gleyze <jonathan.gleyze.pro@gmail.com>
      *
      * @return Template
      */
 	public function settingAction()
     {
-    	 return [];
+        $user = $this->getUser();
+
+        $formFactory = $this->get('fos_user.change_password.form.factory');
+
+        $form = $formFactory->createForm();
+
+        $form->setData($user);
+
+        return[
+            'form' => $form->createView()
+        ];
     }
 }
